f2998bfbe403f8c34aa7c7d16d121851bdac35cd
sergey pavlov <Cap.obvios@gmail.com>
2017-11-15 04:26:28 +0300

    Files explanation

--- 3DManipulator/MyForm.cpp
+++ 3DManipulator/MyForm.cpp
@@ -10,6 +10,12 @@ Author: Sergey Pavlov
 Seminar 4-6
 VS2015/17 (v140 toolkit & windows 8 compatible)
 10.04.2017
+
+Figures folder contain geometry figures. First all the points are states and then the poligons are given.
+vertex.h - vertex data type
+Drawer & GeometryTypes - seminar 1-2 files with 2d graphics logic
+Types - contains data typs necessary for working with 3d data.
+MyForm - main window
 */
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
--- 3DManipulator/polimath.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Types.h"
-
-//namespace polimath
-//{
-
-//}
