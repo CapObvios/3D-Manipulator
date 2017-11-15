7af7baa861b8b696dfd654db1ee6d3fc7356267a
sergey pavlov <Cap.obvios@gmail.com>
2017-11-15 04:22:36 +0300

    Comments + about

--- 3DManipulator/MyForm.cpp
+++ 3DManipulator/MyForm.cpp
@@ -5,6 +5,13 @@
 using namespace System;
 using namespace My3DManipulator;
 
+/*
+Author: Sergey Pavlov
+Seminar 4-6
+VS2015/17 (v140 toolkit & windows 8 compatible)
+10.04.2017
+*/
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	Application::EnableVisualStyles();
